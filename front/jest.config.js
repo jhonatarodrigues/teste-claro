08dd1630b4140874ed269f9da0cf0479c23227c2
Jhonata Rodrigues <jhonata.a.r@hotmail.com>
2026-05-21 19:05:16 -0300

    feat: change npm to pnpm

--- front/jest.config.js
+++ front/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'jest-expo',
   testMatch: ['**/__tests__/**/*.test.ts?(x)'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|expo(nent)?|expo-modules-core|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|nativewind|react-native-css-interop)/)',
+    'node_modules/(?!.pnpm|react-native|@react-native|expo(nent)?|expo-modules-core|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|nativewind|react-native-css-interop)',
+    'node_modules/.pnpm/(?!(react-native|@react-native\\+.*|expo(nent)?|expo-modules-core|@expo\\+.*|@react-navigation\\+.*|nativewind|react-native-css-interop)@)',
   ],
 };
